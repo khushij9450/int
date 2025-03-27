d1ade86ba337cdfffcbf6837036533f3c9e6b5e6
Palak Jaiswal <palakj.ug23.cs@nitp.ac.in>
2025-03-27 18:12:02 +0530

    initial

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 import InterviewCard from '@/components/InterviewCard'
 import { Button } from '@/components/ui/button'
 
-import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
+import { getCurrentUser  } from '@/lib/actions/auth.action'
+import { getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/general.action'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -18,9 +19,9 @@ const page = async() => {
   ]);
  
 
-  const hasPastInterviews = userInterviews?.length !>0;
+  const hasPastInterviews = userInterviews?.length>0;
 
-  const hasUpcomingInterviews = latestInterviews?.length !>0;
+  const hasUpcomingInterviews = latestInterviews?.length>0;
 
   return (
     <>
--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,19 +1,41 @@
-import { cn, getTechLogos } from '@/lib/utils';
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import { cn, getTechLogos } from "@/lib/utils";
 
-const DisplayTechIcons = async ({techStack}: TechIconProps) => {
-    const techIcons=await getTechLogos(techStack);
-  return (
-    <div className='flex flex-row'>{techIcons.slice(0,3).map(({tech, url}, index) =>(
-        <div key={tech}className={cn('relative group bg-dark-300 rounded-full p-2 flex-center', index >=1 && '-ml-3' )}>
-            <span className='tech-tooltip'>{tech}
-
-            </span>
-            <Image src={url} alt={tech} width={100} height={100} className='size-5 '/>
-            </div>
-    ))}</div>
-  )
+interface TechIconProps {
+    techStack: string[];
 }
 
-export default DisplayTechIcons
+const DisplayTechIcons = async ({ techStack }: TechIconProps) => {
+    const techIcons = await getTechLogos(techStack);
+    console.log('techIcons in DisplayTechIcons:', techIcons); // Debug log
+
+    // Fallback if techIcons is empty or not an array
+    if (!Array.isArray(techIcons) || techIcons.length === 0) {
+        return <div>No tech icons available</div>;
+    }
+
+    return (
+        <div className="flex flex-row">
+            {techIcons.slice(0, 3).map(({ tech, url }, index) => (
+                <div
+                    key={tech}
+                    className={cn(
+                        "relative group bg-dark-300 rounded-full p-2 flex flex-center",
+                        index >= 1 && "-ml-3"
+                    )}
+                >
+                    <span className="tech-tooltip">{tech}</span>
+                    <Image
+                        src={url}
+                        alt={tech}
+                        width={100}
+                        height={100}
+                        className="size-5"
+                    />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default DisplayTechIcons;
